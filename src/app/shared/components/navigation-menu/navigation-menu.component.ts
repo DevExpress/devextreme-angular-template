b4bc50c1dfc000399f279b08fce8c07fd7b5664b
ovchinnikov.andrey <ovchinnikov@users.noreply.github.com>
2018-09-14 10:46:51 +0300

    Add compacto mode for menu

--- src/app/shared/components/navigation-menu/navigation-menu.component.ts
+++ src/app/shared/components/navigation-menu/navigation-menu.component.ts
@@ -1,17 +1,14 @@
-import { Component, NgModule, Output, EventEmitter, ViewChild } from '@angular/core';
+import { Component, NgModule, Output, Input, EventEmitter, ViewChild } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DxTreeViewModule, DxTreeViewComponent } from 'devextreme-angular/ui/tree-view';
+
 import { navigation } from '../../../app-navigation';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
     selector: 'app-navigation-menu',
     templateUrl: './navigation-menu.component.html',
-    styles: [`
-        :host {
-            display: block;
-            height: 100%;
-        }
-    `]
+    styleUrls: ['./navigation-menu.component.scss']
 })
 export class NavigationMenuComponent {
     @ViewChild(DxTreeViewComponent)
@@ -20,11 +17,23 @@ export class NavigationMenuComponent {
     @Output() selectedItemChanged = new EventEmitter<string>();
     menuItems: any;
 
+    private _compactMode = false;
+    @Input()
+    get compactMode() {
+        return this._compactMode;
+    }
+    set compactMode(val) {
+        this._compactMode = val;
+        if (val && this.menu.instance) {
+            this.menu.instance.collapseAll();
+        }
+    }
+
     constructor(private router: Router) {
         this.menuItems = navigation;
 
         router.events.subscribe(val => {
-            if (val instanceof NavigationEnd) {
+            if (val instanceof NavigationEnd && this.menu.instance) {
                 this.menu.instance.selectItem(val.url);
             }
         });
@@ -32,15 +41,18 @@ export class NavigationMenuComponent {
 
     onItemSelectionChanged(event) {
         const path = event.itemData.path;
-        if(path) {
+        if (path) {
             this.selectedItemChanged.emit(event.itemData.text);
             this.router.navigate([path]);
+        } else {
+            const treeView = this.menu.instance;
+            event.node.expanded ? treeView.collapseItem(event.itemData) : treeView.expandItem(event.itemData);
         }
     }
 }
 
 @NgModule({
-    imports: [ DxTreeViewModule ],
+    imports: [ CommonModule, DxTreeViewModule ],
     declarations: [ NavigationMenuComponent ],
     exports: [ NavigationMenuComponent ]
 })
