e7299d7e3c1d635bb993d0e335a1bfe346448973
nightskylark <eugen.zha@gmail.com>
2018-10-15 22:35:04 +0300

    Init gh-pages branch

--- src/app/layouts/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './side-nav-outer-toolbar/side-nav-outer-toolbar.component';
-export * from './side-nav-inner-toolbar/side-nav-inner-toolbar.component';
--- src/app/shared/components/index.ts
+++
@@ -1,4 +0,0 @@
-
-export * from './login-form/login-form.component';
-export * from './header/header.component';
-export * from './side-navigation-menu/side-navigation-menu.component';
