5b2262c02690b349b7ad0b41f6ee62867a560af9
Sergey Zvyagin <bingofirst@gmail.com>
2019-06-04 14:05:24 +0300

    Fix the typo

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -20,7 +20,7 @@ export class ProfileComponent {
       BirthDate: new Date('1974/11/15'),
       HireDate: new Date('2005/05/11'),
       /* tslint:disable-next-line:max-line-length */
-      Notes: 'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if yo`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.',
+      Notes: 'Sandra is a CPA and has been our controller since 2008. She loves to interact with staff so if you`ve not met her, be certain to say hi.\r\n\r\nSandra has 2 daughters both of whom are accomplished gymnasts.',
       Address: '4600 N Virginia Rd.'
     };
   }
