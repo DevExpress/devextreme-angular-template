415755f05127e859423a2979aa674fda5b1d07c9
dxvladislavvolkov <vladislav.volkov@devexpress.com>
2019-06-13 12:36:33 +0300

    update-template

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/DevExtreme-App'),
+      dir: require('path').join(__dirname, './coverage/DevExtreme-App'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
