471b259301f2db3abbb9a9943036df7cfbfaf9e9
dxvladislavvolkov <vladislavvolkov@Vladislavs-MacBook-Pro.local>
2021-05-27 16:02:37 +0300

    update-template

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/devextreme-app'),
+      dir: require('path').join(__dirname, './coverage/DevExtreme-App'),
       subdir: '.',
       reporters: [
         { type: 'html' },
